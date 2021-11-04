b85e82384e23267bc5941433d86791362f4bc597
Mark Lee <73801240+brickmeister@users.noreply.github.com>
2021-11-03 22:36:23 -0400

    [FEATURE] Add support for different output directories.

--- src/zeppelin_migration/lib/config_mapper.py
+++ src/zeppelin_migration/lib/config_mapper.py
@@ -1,5 +1,10 @@
 from typing import Dict
 
+"""
+This code creates the mapper for 
+zeppelin interpreters to databricks interpreters
+"""
+
 # valid language conversions from Zeppelin
 config_mapper : Dict[str, str] = {'%pyspark' : '%python',
                                   '%sh'      : '%sh',
